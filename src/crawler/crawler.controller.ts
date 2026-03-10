f600d92a432c8ff3e7bddd49f7ea51fe54eb0182
Dinh Thanh Dat <thanhdat4804@gmail.com>
2026-03-10 10:35:50 +0700

    Fix url

--- src/crawler/crawler.controller.ts
+++ src/crawler/crawler.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { CrawlerService } from './crawler.service'
 
-@Controller('check')
+@Controller('check-violation')
 export class CrawlerController {
 
   constructor(private readonly crawlerService: CrawlerService) {}
